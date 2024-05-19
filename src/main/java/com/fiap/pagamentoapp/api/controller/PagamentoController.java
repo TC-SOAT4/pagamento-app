4569e564bd26363131d28e5c8d10a9a3ee2ba15a
Thiago <thiagobsn@gmail.com>
2024-05-19 20:45:03 -0300

    Ajuste path controller

--- src/main/java/com/fiap/pagamentoapp/api/controller/PagamentoController.java
+++ src/main/java/com/fiap/pagamentoapp/api/controller/PagamentoController.java
@@ -1,19 +1,23 @@
 package com.fiap.pagamentoapp.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fiap.pagamentoapp.api.controller.dto.PagamentoResponse;
 import com.fiap.pagamentoapp.api.controller.dto.ProcessarPagamentoRequest;
 import com.fiap.pagamentoapp.application.mapper.PagamentoMapper;
 import com.fiap.pagamentoapp.application.pagamento.usecases.ProcessarPagamentoUseCase;
 import com.fiap.pagamentoapp.domain.pagamento.entity.Pagamento;
 import com.fiap.pagamentoapp.domain.pagamento.entity.StatusPagamento;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
+@RequestMapping("/pagamentos")
 public class PagamentoController {
 
     private final ProcessarPagamentoUseCase processarPagamentoUseCase;
@@ -23,7 +27,7 @@ public class PagamentoController {
         this.processarPagamentoUseCase = processarPagamentoUseCase;
     }
 
-    @PostMapping("/processar")
+    @PostMapping
     public ResponseEntity<PagamentoResponse> processar(@RequestBody ProcessarPagamentoRequest request) {
         Pagamento pagamento = Pagamento.builder()
                 .idPedido(request.getIdPedido())
