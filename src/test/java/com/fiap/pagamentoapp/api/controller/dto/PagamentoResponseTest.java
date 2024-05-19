7a721396f6a439c34032405bed995a6a1212010d
Thiago <thiagobsn@gmail.com>
2024-05-19 01:05:19 -0300

    Ajuste teste

--- src/test/java/com/fiap/pagamentoapp/api/controller/dto/PagamentoResponseTest.java
+++ src/test/java/com/fiap/pagamentoapp/api/controller/dto/PagamentoResponseTest.java
@@ -8,28 +8,41 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PagamentoResponseTest {
 
     private PagamentoResponse response1;
     private PagamentoResponse response2;
+    private PagamentoResponse response3;
+    private PagamentoResponse response4;
+
 
     @BeforeEach
     void setUp() {
         var data = LocalDateTime.now();
         response1 = new PagamentoResponse("123", 456, new BigDecimal("100.00"), StatusPagamento.PENDENTE, data);
         response2 = new PagamentoResponse("123", 456, new BigDecimal("100.00"), StatusPagamento.PENDENTE, data);
+        response3 = new PagamentoResponse("124", 456, new BigDecimal("100.00"), StatusPagamento.PENDENTE, data);
+        response4 = new PagamentoResponse("123", 457, new BigDecimal("100.00"), StatusPagamento.PENDENTE, data);
+
     }
+    
 
     @Test
     void testEquals() {
         assertEquals(response1, response2);
+        assertNotEquals(response1, response3);
+        assertNotEquals(response1, response4);
     }
 
     @Test
     void testHashCode() {
         assertEquals(response1.hashCode(), response2.hashCode());
+        assertNotEquals(response1.hashCode(), response3.hashCode());
+        assertNotEquals(response1.hashCode(), response4.hashCode());
+        assertNotEquals(response1.hashCode(), new PagamentoResponse().hashCode());
     }
 
     @Test
