5777c778c17565314c675f904b8f5b4a4e63ebeb
Thiago <thiagobsn@gmail.com>
2024-07-31 00:34:05 -0300

    Ajuste fila

--- src/main/java/com/fiap/pagamentoapp/application/pedido/usecase/impl/AtualizarStatusPedidosPagosUseCaseImpl.java
+++ src/main/java/com/fiap/pagamentoapp/application/pedido/usecase/impl/AtualizarStatusPedidosPagosUseCaseImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiap.pagamentoapp.application.pedido.usecase.AtualizarStatusPedidosPagosUseCase;
+import com.fiap.pagamentoapp.application.pedido.usecase.dto.StatusPedidoDTO;
 import com.fiap.pagamentoapp.domain.pagamento.entity.Pagamento;
 
 import io.awspring.cloud.sqs.operations.SqsTemplate;
@@ -24,13 +25,16 @@ public class AtualizarStatusPedidosPagosUseCaseImpl implements AtualizarStatusPe
 
     Logger logger = LoggerFactory.getLogger(AtualizarStatusPedidosPagosUseCaseImpl.class);
 
-    @Value("${aws.sqs.out.pedido-pago.uri}")
+    @Value("${aws.sqs.out.pedido.status.uri}")
     private String uriPedidoPago;
+
+    private static final String STATUS_PAGO = "PAGO";
 
     @Override
     public void executar(Pagamento pagamento) {
         try {
-            String json = objectMapper.writeValueAsString(pagamento);
+            var statusPago = StatusPedidoDTO.builder().idPedido(pagamento.getIdPedido()).status(STATUS_PAGO).build();
+            String json = objectMapper.writeValueAsString(statusPago);
             sqsTemplate.send(uriPedidoPago, MessageBuilder.withPayload(json).build());
             logger.info("Pagamento confirmado. Atualizado status do pedodo: {}", pagamento);
         } catch (JsonProcessingException e) {
